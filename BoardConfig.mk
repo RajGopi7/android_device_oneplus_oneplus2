2565ed5c94d57633f93c08c0d7a1d405cdf030e8
RajGopi7 <mailrajgopi@gmail.com>
2017-01-31 15:25:33 +0000

    no dexpreopt :(

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,15 +160,6 @@ TARGET_NO_RPC := true
 # Sensors
 USE_SENSOR_MULTI_HAL := true
 
-# Enable dexpreopt to speed boot time
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-   endif
-  endif
-endif
-
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HAS_QCOM_WLAN_SDK := true
